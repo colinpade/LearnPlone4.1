58c5292368afedb8e17ba72e3683b47229699cc9
Colin Pade <colin@saucelabs.com>
2015-08-11 10:13:25 -0700

    post chap 8 I think page is off

--- src/optilux.theme/setup.py
+++ src/optilux.theme/setup.py
@@ -1,23 +1,21 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '2.0'
 
 setup(name='optilux.theme',
       version=version,
-      description="",
+      description="Theme for the Optilux website",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
       keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      author='Rob Gietema',
+      author_email='rob@fourdigits.nl',
+      url='http://optilux-cinemas.com',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['optilux'],
@@ -25,18 +23,19 @@ setup(name='optilux.theme',
       zip_safe=False,
       install_requires=[
           'setuptools',
-	  'plone.app.theming',
-          # -*- Extra requirements: -*-
+          'plone.app.theming',
+          'plone.app.themingplugins',
       ],
       extras_require={
-	      'test': ['plone.app.testing',]
+          'test': ['plone.app.testing',]
       },
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-#      setup_requires=["PasteScript"],
-#      paster_plugins=["ZopeSkel"],
+#     entry_points="""
+#     # -*- Entry points: -*-
+#     
+#     [z3c.autoinclude.plugin]
+#     target = plone
+#     """,
+# uncomment these to re-enable support for Paster local commands
+#     setup_requires=["PasteScript"],
+#     paster_plugins=["ZopeSkel"],
       )
